702848013ab54b7ec85378cbea45abd99d5fe83d
David Oliveira <davidoliveira@davioliv01m.corp.skyscanner.local>
2016-03-09 10:37:56 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# node-pzproxy 
+
+A programmable and pluggable HTTP/HTTPS proxy for node.js
+
+## Installing
+
+	npm install pzproxy
+
+## Using it
+
+	"use strict";
+
+	var
+	    PZProxy		= require('../lib/pzproxy');
+
+
+	// Create a proxy instance
+	new PZProxy({
+	    serverOpts: {
+	        port: 9999
+	    },
+	    cacheOpts: {
+	        storage: "./data"
+	    },
+	    proxyOpts: {
+	        target: "http://www.esquerda.net"
+	    },
+	    defaultTTL: 30
+	});
---
+++ bin/server.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+/*
+ TODO:
+ 	- Lock+Queue similar requests
+ 	- Make a map of currently cache item in use and discard them
+ 	- Move cache+proxy login to cacheproxy.js
+ 	- Use SW4 heads for cache key and URL
+ 	- Configurable number of backend connection sockets
+ */
+
+"use strict";
+
+var
+	PZProxy		= require('../lib/pzproxy'),
+	proxy;
+
+
+// Create a proxy instance
+proxy = new PZProxy({
+	serverOpts: {
+		port: 9999
+	},
+	cacheOpts: {
+		storage: "./data"
+	},
+	proxyOpts: {
+		target: "http://www.esquerda.net"
+	},
+	defaultTTL: 30
+});
